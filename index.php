a9a4720fd43b2a105202b147297388d6b97f6253
Lindsay Prahalad <lprahalad@hotmail.co.uk>
2014-12-11 22:21:48 +0000

    My portfolio site

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Lindsay Prahalad: Web Developer - London</title>
+
+<meta name="keywords" content="freelancer, freelance, web, developer, development, London, XHTML, HTML, HTML 5, CSS, jQuery, JavaScript, web design, web development, web site development, web site design, web design development, interactive, london web design, london ecommerce, london e-commerce, london web development, uk, web site, web sites, Lindsay Prahalad">
+
+<meta name="description" content="Lindsay Prahalad is a freelance web developer in London">
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+
+<link rel="stylesheet" type="text/css" href="style/all_pages.css">
+<link rel="stylesheet" type="text/css" href="style/hp_responsive.css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="//use.typekit.net/kbv2zpu.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+<script src="js/about.js"></script>
+
+<!--[if lt IE 9]>
+
+<script>
+document.createElement('header');
+document.createElement('main');
+document.createElement('nav');
+document.createElement('footer');
+document.createElement('small');
+</script>
+
+<![endif]-->
+
+</head>
+
+<body>
+
+<header>
+	<img src="images/hp_header.png" alt="Hello I'm Lindsay Prahalad" />
+</header>
+
+<main id="homepage">
+	<img src="images/hp_hero.png" alt="I'm a web developer from London" />
+	<nav>
+		<a href="portfolio.html">
+			<div id="left_nav_button" class="nav_button">view my work &#9658;</div>
+		</a>
+		<a href="about.html">
+			<div id="right_nav_button" class="nav_button">about me &#9658;</div>
+		</a>
+		<div class="end_of_element"></div>
+	</nav>
+</main>
+
+<footer>
+	<h5>SOCIAL:</h5>
+	<div id="socialIcons">
+		<img src="images/footerLeftBorder.png" alt="" />
+		<a id="fbLink" href="https://www.facebook.com/pages/Paw-Print-Productions/346263595511954" title="Like me on facebook">
+			<img src="images/fbIcon.png" alt="Like me on facebook" />
+		</a>
+		<a id="twitterLink" href="https://twitter.com/lindsayprahalad" title="Tweet me">
+			<img src="images/twitterIcon.png" alt="Tweet me" />
+		</a>
+		<a id="instaLink" href="http://instagram.com/lindsaysprahalad" title="Check out my Instagram">
+			<img src="images/instagramIcon.png" alt="Check out my Instagram" />
+		</a>
+		<a id="linkedInLink" href="http://uk.linkedin.com/pub/lindsay-prahalad/34/935/9a1" title="View my LinkedIn profile">
+			<img src="images/linkedinIcon.png" alt="View my LinkedIn profile" />
+		</a>
+		<img src="images/footerRightBorder.jpg" alt="" />
+	</div>
+	<small>&#169; LINDSAY PRAHALAD 2014</small>
+</footer>
+
+</body>
+</html>	
---
+++ js/about.js
@@ -0,0 +1,104 @@
+var showSkillSection = function() {
+	$('#skillsLink').click(function() {
+		$('#skillsSection').slideToggle('slow', function() {
+			if($('#skillsSection').is( ":visible" )) {
+				$('#skillsLink').html('my skills &#9660;');
+			} else {
+				$('#skillsLink').html('my skills &#9658;');
+			} 
+		});
+	});
+};
+
+var highlightProjectLink = function() {
+	if(ResponsiveHelper.widthOfWindow() > ResponsiveHelper.tabletWidth) {
+		$('#viewProjectLink').addClass('boxedLink nav_button');
+		$('#skillsLink').addClass('boxedLink nav_button');
+	}   
+};
+
+var ResponsiveHelper = {
+	
+	widthOfWindow : function() {	
+		return $(window).width();
+	},
+	
+	tabletWidth : 769,
+	mobileWidth : 625,
+	desktopWidth : 900,
+
+	turnOffBoxedLinksHoverState : function() {
+		if(ResponsiveHelper.widthOfWindow() > ResponsiveHelper.tabletWidth) {
+			return;
+		};
+		
+		$('#viewProjectLink').addClass('boxedLink nav_button highlight_links');
+		$('#skillsLink').addClass('boxedLink nav_button highlight_links');
+	},
+	
+	removeHightlightedlink : function() {
+		if(ResponsiveHelper.widthOfWindow() < ResponsiveHelper.mobileWidth)  {
+			$('#skillsLink').removeClass('highlight_links');
+		};
+	},
+	
+	swapAnimatingScreenForStaticImg : function() {
+		if(ResponsiveHelper.widthOfWindow() > ResponsiveHelper.desktopWidth) {
+			return;
+		}
+		
+		var screenDiv = $('#skills_animated_screen div');
+			
+		$(screenDiv).empty();
+		$(screenDiv).css('margin-top', '20px');
+		$(screenDiv).html('<img src="/images/skills_webdev.png" />');
+	},
+	
+	spotlightDivUpdater: function() {
+		
+		$('.companies').click(function() {
+			$(this).find('details').css('display', 'block');									   
+		});
+	}
+}
+
+var MobileSocialIconsChanger  = {
+	
+	showMobileSocial: function() {
+		
+		if(ResponsiveHelper.widthOfWindow() > ResponsiveHelper.mobileWidth)  {
+			return;
+		}
+		
+		$('footer h5').html('SOCIAL <span>&#9658;</span>');
+		
+		$('footer h5').click(function() {
+			$('#socialIcons').slideToggle('slow', function() {    
+			
+				if($('#socialIcons').is( ":visible" )) {
+					$('footer h5 span').html('&#9660;');
+				} else {
+					$('footer h5 span').html('&#9658;');
+				}
+			});
+		});
+	}
+}
+		
+$(document).ready(function() {
+	showSkillSection();
+	highlightProjectLink();
+	ResponsiveHelper.turnOffBoxedLinksHoverState();
+	ResponsiveHelper.removeHightlightedlink();
+	ResponsiveHelper.swapAnimatingScreenForStaticImg();
+	ResponsiveHelper.spotlightDivUpdater();
+	MobileSocialIconsChanger.showMobileSocial();
+});
+
+$(window).resize(function() {
+	highlightProjectLink();
+	ResponsiveHelper.turnOffBoxedLinksHoverState();
+	ResponsiveHelper.removeHightlightedlink();
+	ResponsiveHelper.swapAnimatingScreenForStaticImg();
+	ResponsiveHelper.spotlightDivUpdater();
+});
