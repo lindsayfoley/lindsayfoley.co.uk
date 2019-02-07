80ddb86e7f583f7e0315e1039e4a715cf0b7c23d
Lindsay Foley <lindsay.foley@disney.com>
2019-02-07 20:37:07 +0000

    Removed stylesheet that was no longer in use

--- structure/styles.php
+++ structure/styles.php
@@ -3,6 +3,5 @@
 <![endif]-->
 <link href="https://fonts.googleapis.com/css?family=Anaheim" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="style/all_pages.min.css">
-<link rel="stylesheet" type="text/css" href="style/hp_responsive.min.css">
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
