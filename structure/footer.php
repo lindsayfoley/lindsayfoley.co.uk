730020ea60b18dc8bc7d7cbb6ad2f037a65e027b
Lindsay Foley <lindsay.foley@disney.com>
2019-04-15 22:11:43 +0100

    Updated Fontawesome link, updated footer icons and added new links

--- structure/footer.php
+++ structure/footer.php
@@ -1,8 +1,10 @@
 <footer>
 	<div id="social-icons">
-		<a id="email-icon" href="mailto:me@lindsayfoley.co.uk" title="Contact me"><i class="fa fa-envelope"></i></a>
-		<a id="linkedin-icon" href="http://uk.linkedin.com/pub/lindsay-foley/34/935/9a1" title="View my LinkedIn profile"><i class="fa fa-linkedin-square"></i></a>
-		<a id="instagram-icon" href="http://instagram.com/comeflywithlindsay" title="Check out my Instagram"><i class="fa fa-instagram"></i></a>
+		<a target="_blank" href="mailto:me@lindsayfoley.co.uk" title="Contact me"><i class="fa fa-paper-plane"></i></a>
+		<a target="_blank" href="http://uk.linkedin.com/pub/lindsay-foley/34/935/9a1" title="View my LinkedIn profile"><i class="fab fa-linkedin-in"></i></a>
+		<a target="_blank" href="https://github.com/lindsayfoley" title="Find me on GitHub"><i class="fab fa-github-alt"></i></a>
+		<a target="_blank" href="http://instagram.com/comeflywithlindsay" title="Check out my Instagram"><i class="fab fa-instagram"></i></a>
+		<a target="_blank" href="http://comeflywithlindsay.com" title="Have a look at my blog"><i class="fas fa-rss"></i></a>	
 	</div>
-	<small>&#169; LINDSAY FOLEY</small>
+	<small><strong>&#169; LINDSAY FOLEY</strong></small>
 </footer>
--- structure/styles.php
+++ structure/styles.php
@@ -3,5 +3,4 @@
 <![endif]-->
 <link href="https://fonts.googleapis.com/css?family=Anaheim" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="style/all_pages.min.css">
-<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
